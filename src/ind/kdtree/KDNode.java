6b5b1d630bd730b7bbf0cfcfd18711e04288e5d6
csam6764 <M.Stanic@student.uibk.ac.at>
2013-11-13 15:00:03 +0100

    Signed-off-by: csam6764 <M.Stanic@student.uibk.ac.at>

--- src/ind/kdtree/KDNode.java
+++ src/ind/kdtree/KDNode.java
@@ -4,26 +4,39 @@ import cbir.image.DescriptorType;
 import cbir.image.Image;
 
 /**
- * Implements a node of a k-d-tree. Every node has a point (image), a dimension
- * and references to the parent and the next nodes.
+ * Implements a node of a k-d-tree. Every node has a point (image), a dimension (k), a depth, a descriptor type 
+ * and references to the parent and the child nodes.
  * 
- * @author Wendler Chris & Stanic Matej
- * 
+ * @author Wendler Chris
+ * @author Stanic Matej
  */
 public class KDNode implements Comparable<KDNode> {
+	/** dimension of the node descriptor */
 	private int k;
+	/** depth of the node point */
 	private int depth;
+	/** image contained in the node*/
 	private Image image = null;
+	/** descriptor type of the node descriptor */
 	private DescriptorType type;
+	/** parent node */
 	private KDNode parent = null;
+	/** left child node */
 	private KDNode lesser = null;
+	/** right child node */
 	private KDNode greater = null;
 
+	/**
+	 * Constructor of KDNode.
+	 */
 	public KDNode(Image image) {
 		super();
 		this.image = image;
 	}
 
+	/**
+	 * Constructor of KDNode.
+	 */
 	public KDNode(int dim, int depth, Image image, DescriptorType type) {
 		this(image);
 		this.k = dim;
@@ -31,6 +44,20 @@ public class KDNode implements Comparable<KDNode> {
 		this.type = type;
 	}
 
+	/**
+	 * Compares the values of the actual splitting coordinate of two node descriptors.
+	 * @param depth
+	 * 			the depth of the actual node
+	 * @param k
+	 * 			the dimension of the actual node descriptor 
+	 * @param image1
+	 * 			the actual node image
+	 * @param image2
+	 * 			the node image which is compared to
+	 * @param type
+	 * 			the descriptor type
+	 * @return
+	 */
 	public static int compareTo(int depth, int k, Image image1, Image image2,
 			DescriptorType type) {
 		int dim = depth % k;
@@ -43,6 +70,12 @@ public class KDNode implements Comparable<KDNode> {
 		return 0;
 	}
 
+	/**
+	 * Compares the values of the actual splitting coordinate of a specified node descriptor 
+	 * to the actual node descriptor.
+	 * @param node
+	 * 			the node that is compared with the actual node.
+	 */
 	@Override
 	public int compareTo(KDNode node) {
 		return compareTo(depth, k, image, node.getImage(), type);
